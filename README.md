2e5d2a43fae4b812aad2b14201417ac980b0ddc3
Jaimeson Waugh <jaimeson@4cesi.com>
2019-06-04 13:44:47 -0700

    Update TestHashItem dependency

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ WP Post Link
 =========================
 
 A simpler way to get link HTML by passing a post slug, ID, or WP_Post object.
+
+## Changelog
+
+### 0.2.0
+* Add PostType
+
+### 0.1.0
+* Create initial usable release
--- src/WPPostLink.php
+++ src/WPPostLink.php
@@ -1,49 +1,47 @@
 <?php
 
 declare( strict_types = 1 );
-namespace WaughJ\WPPostLink
+namespace WaughJ\WPPostLink;
+
+use WaughJ\HTMLLink\HTMLLink;
+use WaughJ\TestHashItem\TestHashItem;
+
+class WPPostLink extends HTMLLink
 {
-	use WaughJ\HTMLLink\HTMLLink;
-	use function WaughJ\TestHashItem\TestHashItemExists;
-	use function WaughJ\TestHashItem\TestHashItemString;
-
-	class WPPostLink extends HTMLLink
+	public function __construct( array $atts )
 	{
-		public function __construct( array $atts )
+		$post = ( isset( $atts[ 'post' ] ) && is_a( $atts[ 'post' ], '\WP_Post' ) ) ? $atts[ 'post' ] : self::getPage( $atts );
+		if ( $post !== null )
 		{
-			$post = ( isset( $atts[ 'post' ] ) && is_a( $atts[ 'post' ], '\WP_Post' ) ) ? $atts[ 'post' ] : self::getPage( $atts );
-			if ( $post !== null )
-			{
-				$url = get_permalink( $post );
-				$value = TestHashItemExists( $atts, 'value', $post->post_title );
-			}
-			else
-			{
-				$url = TestHashItemString( $atts, 'slug', '' );
-				$value = TestHashItemExists( $atts, 'value', $url );
-			}
-			parent::__construct( $url, $value, $atts );
+			$url = get_permalink( $post );
+			$value = $atts[ 'value' ] ?? $post->post_title;
 		}
-
-		private static function getPage( array $atts )
+		else
 		{
-			$post_type = TestHashItemString( $atts, 'post_type', get_post_types() );
-			if ( TestHashItemString( $atts, 'slug' ) )
-			{
-				return get_page_by_path( $atts[ 'slug' ], OBJECT, $post_type );
-			}
-			else if ( isset( $atts[ 'post_id' ] ) )
-			{
-				return get_post( intval( $atts[ 'post_id' ] ) );
-			}
-			else if ( TestHashItemString( $atts, 'post_title' ) )
-			{
-				return get_page_by_title( $atts[ 'post_title' ], OBJECT, $post_type );
-			}
-			else
-			{
-				return null;
-			}
+			$url = TestHashItem::getString( $atts, 'slug', '' );
+			$value = $atts[ 'value' ] ?? $url;
+		}
+		parent::__construct( $url, $value, $atts );
+	}
+
+	private static function getPage( array $atts )
+	{
+		$post_type = TestHashItem::getString( $atts, 'post_type', get_post_types() );
+		if ( TestHashItem::getString( $atts, 'slug' ) )
+		{
+			return get_page_by_path( $atts[ 'slug' ], OBJECT, $post_type );
+		}
+		else if ( isset( $atts[ 'post_id' ] ) )
+		{
+			return get_post( intval( $atts[ 'post_id' ] ) );
+		}
+		else if ( TestHashItem::getString( $atts, 'post_title' ) )
+		{
+			return get_page_by_title( $atts[ 'post_title' ], OBJECT, $post_type );
+		}
+		else
+		{
+			return null;
 		}
 	}
 }
